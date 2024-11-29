3f0b4e2ab4f84cd711bcb010f308d5bd62da8780
nghia <dtn04999@gmail.com>
2024-11-30 01:32:38 +0700

    nghianghianghianghia

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -37,6 +37,7 @@ function Login() {
       try {
         const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/login`, {
           method: "POST",
+          mode: "no-cors",
           credentials: "include", // Nếu dùng cookie
           headers: {
             "Content-Type": "application/json",
