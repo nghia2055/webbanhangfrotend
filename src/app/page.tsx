4feb17a2ad5f45fd9547ab4ba1d7d0781c389b12
nghia2055 <dtn0499@gmail.com>
2024-12-12 19:41:20 +0700

    1321321321321dasdas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,8 +15,7 @@ type data = {
   _id: string;
 };
 
-export default async function Home() {
-  console.log(process.env.NEXT_PUBLIC_API_URL);
+export default async function Page() {
   const getProductHome = async () => {
     try {
       const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/producthome`);
