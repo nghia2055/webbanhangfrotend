080986464c6c3d2c72315d4ece028e4f667f91f0
nghia2055 <dtn0499@gmail.com>
2024-12-12 19:54:02 +0700

    hah123123

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,68 +16,70 @@ type data = {
 };
 
 export default async function Page() {
-  const getProductHome = async () => {
-    try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/producthome`);
-      const data = await res.json();
-      return data;
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  const data = await getProductHome();
+  // const getProductHome = async () => {
+  //   try {
+  //     const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/producthome`);
+  //     const data = await res.json();
+  //     return data;
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
+  // const data = await getProductHome();
+  const data: [] = [];
 
   return (
     <>
       <Header />
       <main className="max-w-[1480px] mx-auto md:pt-48 grid gap-y-6 ">
         <section className="grid grid-cols-5 gap-x-10 pb-10">
-          {data?.map((item: data, index: number) => {
-            const price = String(item.price).replace(
-              /\B(?=(\d{3})+(?!\d))/g,
-              "."
-            );
-            const src = item.productImages[4];
-            return (
-              <div
-                className=" w-full h-auto border-[1px] flex flex-col items-center"
-                key={index}
-              >
-                <div className="flex flex-col items-center ">
-                  <Link
-                    href={
-                      item.collection && item._id
-                        ? `/collection/${item.collection}/${item._id}`
-                        : "/"
-                    }
-                  >
-                    <Image
-                      src={src}
-                      width={500}
-                      height={400}
-                      alt="giày"
-                      className=" hover:scale-x-[-1] hover:opacity-50  "
-                    />
-                  </Link>
-                  <Link
-                    href={item.collection ? `/${item.collection}` : "/"}
-                    className="text-xs bg-[rgb(249,248,248)] w-full text-center opacity-55"
-                  >
-                    {item.collection}
-                  </Link>
-                </div>
-                <div className="mt-2 text-center space-y-3">
-                  <h2 className="text-sm opacity-80 leading-none">
-                    {item.productName}
-                  </h2>
-                  <div className="text-md text-red-700 font-bold">
-                    {price}
-                    <span className="underline">đ</span>
+          {data &&
+            data?.map((item: data, index: number) => {
+              const price = String(item.price).replace(
+                /\B(?=(\d{3})+(?!\d))/g,
+                "."
+              );
+              const src = item.productImages[4];
+              return (
+                <div
+                  className=" w-full h-auto border-[1px] flex flex-col items-center"
+                  key={index}
+                >
+                  <div className="flex flex-col items-center ">
+                    <Link
+                      href={
+                        item.collection && item._id
+                          ? `/collection/${item.collection}/${item._id}`
+                          : "/"
+                      }
+                    >
+                      <Image
+                        src={src}
+                        width={500}
+                        height={400}
+                        alt="giày"
+                        className=" hover:scale-x-[-1] hover:opacity-50  "
+                      />
+                    </Link>
+                    <Link
+                      href={item.collection ? `/${item.collection}` : "/"}
+                      className="text-xs bg-[rgb(249,248,248)] w-full text-center opacity-55"
+                    >
+                      {item.collection}
+                    </Link>
+                  </div>
+                  <div className="mt-2 text-center space-y-3">
+                    <h2 className="text-sm opacity-80 leading-none">
+                      {item.productName}
+                    </h2>
+                    <div className="text-md text-red-700 font-bold">
+                      {price}
+                      <span className="underline">đ</span>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
         </section>
       </main>
       <Footer />
