d424066d17b9ca6ddfe7d91a7e7f571edd6147d6
nghia2055 <dtn0499@gmail.com>
2024-12-16 21:29:22 +0700

    update1120021021

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -16,7 +16,7 @@ export async function generateStaticParams() {
   }));
 }
 
-const blog = async ({ params }: { params: { slug: string } }) => {
+const blog = async ({ params }: { params: Promise<{ slug: string }> }) => {
   const param = await params;
   const name = param.slug;
 
