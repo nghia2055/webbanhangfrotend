8731eeea4642ebe08ba477a5c10c312fab03481e
nghia2055 <dtn0499@gmail.com>
2024-12-16 21:18:46 +0700

    update13213123

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
+import type { NextConfig } from "next";
 
-const nextConfig = {
+const nextConfig: NextConfig = {
+  /* config options here */
   images: {
     domains: [
       "www.blissworld.com",
@@ -12,4 +13,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
