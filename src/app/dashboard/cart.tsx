f8d1b977e01041fbc0b76c2b12b4d078e3ec00ed
nghia2055 <dtn0499@gmail.com>
2024-12-19 20:41:18 +0700

    update1321231312321

--- src/app/dashboard/cart.tsx
+++ src/app/dashboard/cart.tsx
@@ -26,9 +26,18 @@ function Cart() {
     const fetchOrder = async () => {
       try {
         const res = await fetch(`/api/auth/dashboardorder`);
-        const data = await res.json();
-        console.log(data);
-        setData(data);
+        const dataJSON = await res.json();
+        const processedData = dataJSON.map((item: any) => {
+          if (item.product) {
+            try {
+              item.product = JSON.parse(item.product); // Chuyển chuỗi JSON thành đối tượng
+            } catch (error) {
+              console.error("Error parsing JSON:", error);
+            }
+          }
+          return item;
+        });
+        setData(processedData);
       } catch (err) {
         console.log(err);
       }
@@ -36,54 +45,53 @@ function Cart() {
     fetchOrder();
   }, []);
 
+  console.log("data", data);
   return (
     <div className="text-center flex justify-center text-xl ">
       <main>
         <section className="grid md:grid-cols-4 md:gap-x-10 md:pb-10 grid-cols-3 lg:grid-cols-5">
           {Array.isArray(data) &&
             data?.map((item: any, index: number) => {
-              return JSON.parse(item.product).map(
-                (item: product, index: number) => {
-                  const price = String(item.price).replace(
-                    /\B(?=(\d{3})+(?!\d))/g,
-                    "."
-                  );
-                  return (
-                    <>
-                      <div
-                        className=" w-full h-auto border-[1px] flex flex-col items-center"
-                        key={`${item._id}-${index}`}
-                      >
-                        <div className="flex flex-col items-center ">
-                          <Image
-                            priority
-                            src={item.productImages}
-                            width={500}
-                            height={400}
-                            alt="giày"
-                            className=" hover:scale-x-[-1] hover:opacity-50  "
-                          />
+              return item.product.map((item: product, index: number) => {
+                const price = String(item.price).replace(
+                  /\B(?=(\d{3})+(?!\d))/g,
+                  "."
+                );
+                return (
+                  <>
+                    <div
+                      className=" w-full h-auto border-[1px] flex flex-col items-center"
+                      key={`${item._id}-${index}`}
+                    >
+                      <div className="flex flex-col items-center ">
+                        <Image
+                          priority
+                          src={item.productImages}
+                          width={500}
+                          height={400}
+                          alt="giày"
+                          className=" hover:scale-x-[-1] hover:opacity-50  "
+                        />
 
-                          <span className="text-xs bg-[rgb(249,248,248)] w-full text-center opacity-55">
-                            {item.size}
+                        <span className="text-xs bg-[rgb(249,248,248)] w-full text-center opacity-55">
+                          {item.size}
+                        </span>
+                      </div>
+                      <div className="mt-2 text-center ">
+                        <h2 className="text-sm opacity-80 leading-none">
+                          <span className="line-clamp-2 break-all ">
+                            {item.name}
                           </span>
-                        </div>
-                        <div className="mt-2 text-center ">
-                          <h2 className="text-sm opacity-80 leading-none">
-                            <span className="line-clamp-2 break-all ">
-                              {item.name}
-                            </span>
-                          </h2>
-                          <div className="md:text-md text-xs text-red-700 font-bold">
-                            {price}
-                            <span className="underline">đ</span>
-                          </div>
+                        </h2>
+                        <div className="md:text-md text-xs text-red-700 font-bold">
+                          {price}
+                          <span className="underline">đ</span>
                         </div>
                       </div>
-                    </>
-                  );
-                }
-              );
+                    </div>
+                  </>
+                );
+              });
             })}
         </section>
       </main>
